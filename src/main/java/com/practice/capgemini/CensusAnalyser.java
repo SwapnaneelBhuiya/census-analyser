6618eb47c791f339ac790f2e0c9176ac03492c71
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-10-20 12:39:33 +0530

    [Swapnaneel]REFACTORED: Extract data from CSV file reducing DRY violations

--- src/main/java/com/practice/capgemini/CensusAnalyser.java
+++ src/main/java/com/practice/capgemini/CensusAnalyser.java
@@ -14,11 +14,11 @@ public class CensusAnalyser {
             CsvToBean<IndiaCensusCSV> csvToBean;Reader reader =null;
             try {
                 reader = Files.newBufferedReader(Paths.get(csvFilePath));
-                CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-                csvToBeanBuilder.withType(IndiaCensusCSV.class);
-                csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-                csvToBean = csvToBeanBuilder.build();
-                Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+//                CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+//                csvToBeanBuilder.withType(IndiaCensusCSV.class);
+//                csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+//                csvToBean = csvToBeanBuilder.build();
+                Iterator<IndiaCensusCSV> censusCSVIterator = this.getCSVFileIterator(reader,  IndiaCensusCSV.class);
                 Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
                 int namOfEateries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
                 return namOfEateries;
@@ -43,12 +43,12 @@ public class CensusAnalyser {
         public int loadIndianStateCode(String csvFilePath) throws CensusAnalyserException {
             CsvToBean<IndiaStateCodeCSV> csvToBean;Reader reader =null;
             try {
-                reader= Files.newBufferedReader(Paths.get(csvFilePath));
-                CsvToBeanBuilder<IndiaStateCodeCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-                csvToBeanBuilder.withType(IndiaStateCodeCSV.class);
-                csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-                csvToBean=csvToBeanBuilder.build();
-                Iterator<IndiaStateCodeCSV> censusCSVIterator = csvToBean.iterator();
+                  reader= Files.newBufferedReader(Paths.get(csvFilePath));
+//                CsvToBeanBuilder<IndiaStateCodeCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+//                csvToBeanBuilder.withType(IndiaStateCodeCSV.class);
+//                csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+//                csvToBean=csvToBeanBuilder.build();
+                Iterator<IndiaStateCodeCSV> censusCSVIterator = this.getStateCSVFileIterator(reader,IndiaStateCodeCSV.class);
                 Iterable<IndiaStateCodeCSV> csvIterable=()-> censusCSVIterator;
                 int namOfEateries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
                 return namOfEateries;
@@ -68,4 +68,65 @@ public class CensusAnalyser {
                 catch(Exception e){}
             }
         }
+        private <E> Iterator<IndiaCensusCSV> getCSVFileIterator(Reader reader, Class csvClass) throws CensusAnalyserException {
+            CsvToBean<IndiaCensusCSV> csvToBean;
+            try {
+                CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+                csvToBeanBuilder.withType(IndiaCensusCSV.class);
+                csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+                csvToBean = csvToBeanBuilder.build();
+                //Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+                //Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
+                return csvToBean.iterator();
+//                while (censusCSVIterator.hasNext()) {
+//                    namOfEateries++;
+//                    IndiaCensusCSV censusData = censusCSVIterator.next();
+//                }
+            }
+            catch(IllegalStateException e) {
+                throw new CensusAnalyserException(e.getMessage(),
+                        CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+            }catch (RuntimeException e) {
+                throw new CensusAnalyserException(e.getMessage(),
+                        CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+            }finally {
+                try {
+                    reader.close();
+                }
+                catch(Exception e){}
+            }
+        }
+    private <E> Iterator<IndiaStateCodeCSV> getStateCSVFileIterator(Reader reader, Class csvClass) throws CensusAnalyserException {
+        CsvToBean<IndiaStateCodeCSV> csvToBean;
+        try {
+            CsvToBeanBuilder<IndiaStateCodeCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(csvClass);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            csvToBean = csvToBeanBuilder.build();
+            //Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+            //Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
+            return csvToBean.iterator();
+//                while (censusCSVIterator.hasNext()) {
+//                    namOfEateries++;
+//                    IndiaCensusCSV censusData = censusCSVIterator.next();
+//                }
+        } catch (IllegalStateException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+        } catch (RuntimeException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        } finally {
+            try {
+                reader.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+    private <E> int getCount(Iterator<E> iterator)
+    {
+        Iterable csvIterable=()-> iterator;
+        int namOfEateries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
+        return namOfEateries;
+    }
 }
